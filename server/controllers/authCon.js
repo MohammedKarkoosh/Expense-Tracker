0a7fc6bd35493a7a45a1eae3dc77589a8cb6a148
MohammedKarkoosh <mohammedkakkroosh@gmail.com>
2025-06-26 23:41:33 -0500

    created a asynchronous Express route handler  to handle user registrations

--- server/controllers/authCon.js
+++ server/controllers/authCon.js
@@ -5,27 +5,40 @@ export const signUpUser = async (req, res)=>{
 
     try {
         
-        const {firstName, email, passowrd} = req.body;
-        if(!(firstName || email || passowrd)){
+        const {firstName, email, password} = req.body;
+        if(!(firstName || email || password)){
             return res.status(404).json({
                 status: "failed",
                 message: "Please Provide Required Fields",
             });
         }
 
-        const user = await pool.query({
+        const userExist = await pool.query({
             text: "SELECT EXISTS (SELECT * FROM tbluser WHERE email = $1)",
             values: [email],
         });
 
-        if(user.rows[0].userExist){
+        if(userExist.rows[0].userExist){
             return res.status(409).json({
                 status: "failed",
                 message: "User Already  Exists",
             });
         }
 
-        // const hashPassword = 
+        const hashPassword = await hashPassword(password);
+        const user = await pool.query({
+            text: "INSERT INTO tbluser (firstname, email, password) VALUES ($1, $2, $3) RETURNING *",
+            values: [firstName, email, hashPassword] 
+        });
+
+        user.rows[0].password = undefined;
+
+        res.status(201).json({
+            status: "success",
+            message: "User Account Created Successfully",
+            user: user.rows[0]
+        });
+
     } catch (error) {
         console.log(error);
         res.status(500).json({
--- server/libs/index.js
+++ server/libs/index.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import JWT from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 export const hashPassword = async (userVal) =>{
     
@@ -8,4 +10,27 @@ export const hashPassword = async (userVal) =>{
     const hashPassword = await bcrypt.hashPassword(userVal, salt);
 
     return hashPassword;
+};
+
+export const passwordCompare = async (userPass, passwordFromDb)=>{
+
+    try {
+        const isMatch = await bcrypt.compare(userPass, passwordFromDb);
+
+        return isMatch;        
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const createJWT = (userId)=>{
+
+    return JWT.sign(
+        {
+            userId: userId
+        }, 
+        process.env.JWT_SECRET, 
+        {
+            expiresIn: '1d',
+        });
 };
