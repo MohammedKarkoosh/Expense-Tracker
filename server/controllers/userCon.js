34a6f1c76471b8408818c7eb8ec2980b1fa04db0
MohammedKarkoosh <mohammedkakkroosh@gmail.com>
2025-06-29 00:24:24 -0500

    Completed controller to get all users

--- server/controllers/userCon.js
+++ server/controllers/userCon.js
@@ -1,52 +1,54 @@
+import { pool } from "../libs/db.js";
 
+export const getUser = async (req, res) => {
+  try {
+    const { userId } = req.body.user;
 
-export const getUser=async(req, res)=>{
+    const isUser = await pool.query({
+      text: "SELECT * FROM tbluser WHERE id = $1",
+      values: [userId],
+    });
 
+    const user = isUser.rows[0];
 
-    try {
-        
-
-        
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            status: "Failed",
-            message: error.message
-        })
+    if (!user) {
+      return res.status(401).json({
+        status: "Failed",
+        message: "No User Found",
+      });
     }
+    user.password = undefined;
+    res.status(201).json({
+      status: "Success",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+};
 
+export const updateUser = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+};
 
-}
-
-export const updateUser=async(req, res)=>{
-
-
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            status: "Failed",
-            message: error.message
-        })
-    }
-
-
-}
-
-
-export const changePassword=async(req, res)=>{
-
-
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            status: "Failed",
-            message: error.message
-        })
-    }
-
-
-}
+export const changePassword = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: "Failed",
+      message: error.message,
+    });
+  }
+};
