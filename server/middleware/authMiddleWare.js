170cf569fd89b36c30514ab00417352768ec17fb
MohammedKarkoosh <mohammedkakkroosh@gmail.com>
2025-06-29 00:11:43 -0500

    updated AuthMiddleWare

--- server/middleware/authMiddleWare.js
+++ server/middleware/authMiddleWare.js
@@ -3,14 +3,32 @@ import JWT from "jsonwebtoken";
 
 const authMiddleWare = async(req, res, next)=>{
 
-    const authHeaderToken = req?.header?.authorization;
+    const authHeaderToken = req.headers.authorization;
 
-    if (!authHeaderToken || !authHeaderToken?.startWith("Bearer")){
+    if (!authHeaderToken || !authHeaderToken?.startsWith("Bearer ")){
+        // "Bearer" is a type that tells the server how to interpret the token.
         return res.status(401).json({
-            status: "auth_failed"
+            status: "auth_failed",
+            message:"Authroization Fialed"
         })
     }
+    // Splits the string to extract the actual JWT, which follows the word "Bearer"
+    const authToken = authHeaderToken?.split(" ")[1];
 
+    try {
+        const decoded = JWT.verify(authToken, process.env.JWT_SECRET);
 
-
+        req.user = {
+            userId:decoded.userId,
+        }
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({
+            status: "auth_failed",
+            message: "Invalid or expired token "
+        })
+    }
 }
+
+export default authMiddleWare;
