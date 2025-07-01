a76c002d49b5c1bec1cc4c3669c3ab3640837b8c
MohammedKarkoosh <mohammedkakkroosh@gmail.com>
2025-07-01 01:08:22 -0500

    Update accountCon.js

--- server/controllers/accountCon.js
+++ server/controllers/accountCon.js
@@ -30,6 +30,10 @@ export const getAccount = async (req, res) => {
 
 export const addMoneyToAccount = async (req, res) => {
   try {
+    const {userId} = req.user;
+    const {id} = req.params;
+    const {amount} = req.body;
+    
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -57,9 +61,28 @@ export const createAccount = async (req, res) => {
     }
 
     const createAccounts = await pool.query({
-        text:"INSERT INTO tblaccount(user_id, account_name, account_number, account_balance) VALUES($1, $2, $3, $4)",
+        text:"INSERT INTO tblaccount(user_id, account_name, account_number, account_balance) VALUES($1, $2, $3, $4) RETURNING *",
         values:[userId, account_name, account_number, account_balance]
-    })
+    });
+    const account = createAccounts.rows[0];
+    const userAccounts = Array.isArray(userName)? userName : [userName];
+    const updateUserAccountsQuery = {
+        text:"UPDATE tbluser SET accounts = array_cat(accounts, $1), updatedate = CURRENT_TIMESTAMP WHERE id=$2 RETURNING *",
+        values:[userAccounts,userId]
+    };
+    await pool.query(updateUserAccountsQuery);
+    const desc = account.account.account_name+" (Initail Deposit)";
+
+    const InitailDeposite = await pool.query({
+        text:"INSERT INTO tbltransaction(user_id, description, type, status, amount, source) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+        values:[userId, desc, "Icome","Completed", account_balance, account.account_name]
+    });
+    res.status(200).JSON({
+      status: "Success",
+      message: "Account Created " + account.account_name,
+      data: account,
+    });
+
   } catch (error) {
     console.log(error);
     res.status(500).json({
