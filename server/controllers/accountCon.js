9045c0508c4524a698f6b595322d7b749952a34a
MohammedKarkoosh <mohammedkakkroosh@gmail.com>
2025-06-30 23:30:32 -0500

    Update accountCon.js

--- server/controllers/accountCon.js
+++ server/controllers/accountCon.js
@@ -1,50 +1,70 @@
 import express from "express";
+import { pool } from "../libs/db";
 
+export const getAccount = async (req, res) => {
+  try {
+    const { userId } = req.user;
 
-
-export const getAccount=(res, req)=>{
-
-    try {
-        const {accountId} = req.accountId;
-        if(!accountId){
-            return res.status(404).JSON({
-                status:"Fail",
-                message: "Account Not Found"
-            });
-        }
-
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            status: "FAILED",
-            message: error.message,
-        });
+    const getAccounts = await pool.query({
+      text: "SELECT * FROM tblaccount WHERE user_id = $1",
+      values: [userId],
+    });
+    if (!userId) {
+      return res.status(404).JSON({
+        status: "Fail",
+        message: "User Not Found",
+      });
     }
-}
+    res.status(200).JSON({
+      status: "Success",
+      data: getAccounts.rows,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "FAILED",
+      message: error.message,
+    });
+  }
+};
 
-export const addMoneyToAccount=(res, req)=>{
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            status: "FAILED",
-            message: error.message,
-        });
+export const addMoneyToAccount = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "FAILED",
+      message: error.message,
+    });
+  }
+};
+
+export const createAccount = async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const { account_name, account_balance, account_number } = req.body;
+
+    const isAccount = await pool.query({
+      text: "SELECT * FROM tblaccount WHERE account_name = $1 AND user_id=$2",
+      values: [account_name, userId],
+    });
+    const accountExist = isAccount.rows[0];
+    if (isAccount) {
+      res.status(409).json({
+        status: "FAILED",
+        message: "Account Exists Already",
+      });
     }
-}
 
-export const createAccount=(res, req)=>{
-
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            status: "FAILED",
-            message: error.message,
-        });
-    }
-}
+    const createAccounts = await pool.query({
+        text:"INSERT INTO tblaccount(user_id, account_name, account_number, account_balance) VALUES($1, $2, $3, $4)",
+        values:[userId, account_name, account_number, account_balance]
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: "FAILED",
+      message: error.message,
+    });
+  }
+};
